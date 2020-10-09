6ccb96f0871604efd33572e3d39fc54d1411e82c
zolachu <zolboo@stanford.edu>
2020-10-08 22:21:44 -0700

    bitConverter

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { TabView, TabBar } from "react-native-tab-view";
+import convert from "convert-units";
+import Constants from "expo-constants";
+
+const measures = convert().measures();
+const mainColor = "#FF69B4";
+const MeasureView = ({ measure }) => <Text>{measure}</Text>;
 
 export default function App() {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState(measures.map((m) => ({ key: m, title: m })));
+
+  const renderScene = ({ route }) => {
+    return <MeasureView measure={route.key} />;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.scene, { marginTop: Constants.statusBarHeight }]}>
+      <Text>bit Converter app!</Text>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: Dimensions.get("window").width }}
+        renderTabBar={(props) => (
+          <TabBar
+            {...props}
+            scrollEnabled
+            tabStyle={{ width: "auto" }}
+            indicatorStyle={{ backgroundColor: "#DC143C" }}
+            style={{ backgroundColor: mainColor }}
+          />
+        )}
+      ></TabView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  scene: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
